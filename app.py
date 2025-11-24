dbb2f9d678febd5dec5efcbc2445fcf761717107
workdoljw <yywongbm@connect.ust.hk>
2025-11-24 14:10:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
 
+# import streamlit as st
+# from streamlit_option_menu import option_menu
+
+# st.title('Hello, Students!')
+# st.write('This is your Python Programming course.')
+
+# with st.sidebar:
+#     selected=option_menu(
+#         menu_title = "Menu",
+#         options = ["Home", "About", "Contact"],
+#         icons = ["house",
+#                  "2-circle-fill",
+#                  "3-circle-fill"],
+#         menu_icon= "emoji-smile-fill",
+#         default_index=0,
+#     )
+
+# if selected == "Home":
+#     st.title(f"Welcome to the {selected} page.")
+
+# if selected == "About":
+#     st.title(f"Welcome to the {selected} page.")
+
+# if selected == "Contact":
+#     st.title(f"Welcome to the {selected} page.")
 import streamlit as st
-from streamlit_option_menu import option_menu
+import pandas as pd
+import os
 
-st.title('Hello, Students!')
-st.write('This is your Python Programming course.')
+# Get the current working directory
+current_directory = os.getcwd()
+# Define the file path
+file_path = os.path.join(current_directory, 'winequality-red.csv')
 
-with st.sidebar:
-    selected=option_menu(
-        menu_title = "Menu",
-        options = ["Home", "About", "Contact"],
-        icons = ["house",
-                 "2-circle-fill",
-                 "3-circle-fill"],
-        menu_icon= "emoji-smile-fill",
-        default_index=0,
-    )
+# Read the CSV file into a DataFrame
+df = pd.read_csv(file_path, delimiter=';')
 
-if selected == "Home":
-    st.title(f"Welcome to the {selected} page.")
-
-if selected == "About":
-    st.title(f"Welcome to the {selected} page.")
-
-if selected == "Contact":
-    st.title(f"Welcome to the {selected} page.")
+# Display the DataFrame in an interactive table
+st.write("Wine Quality Data")
+st.dataframe(df)
